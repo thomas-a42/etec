a35eb7a295cf04cff1333a1c266c9b0a25f8fe38
Thomas A. <152197213+thomas-a42@users.noreply.github.com>
2026-04-06 09:34:32 -0300

    Update validarLogin function for welcome message

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 function validarLogin() {
     let login = document.getElementById("login").value;
-    alert(`ERRO\num usuário já fez esse login\ntentativa de login: ${login}`);
+    alert("Seja bem vindo, " + login);
 }
