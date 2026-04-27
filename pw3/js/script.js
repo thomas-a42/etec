8eee4de00d5a49508792c06d7b0bfe433867f288
Thomas A. <152197213+thomas-a42@users.noreply.github.com>
2026-04-27 11:02:17 -0300

    Enhance login validation with password check

--- pw3/js/script.js
+++ pw3/js/script.js
@@ -1,4 +1,11 @@
-function validarLogin() {
-    let login = document.getElementById("login").value;
-    alert("Seja bem vindo, " + login);
+function validarLogin(){
+    let login = document.getElementById('login').value; 
+    let senha = document.getElementById('senha').value; 
+
+    if (login === 'admin' && senha === 'senhafoda') {
+        alert("Seja bem vindo, " + login);
+        window.location.href = 'produtos.html'
+    } else {
+        alert(`Usuário ou senha inválidos, tente novamente.`);
+    }   
 }
